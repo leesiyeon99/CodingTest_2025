2fa2cc5a95c63333dc8ba94773ea71a6ac59ea4b
leesiyeon99 <qsqs9090@naver.com>
2025-02-26 21:20:10 +0900

    Feat : 문제10

--- Problem1/obj/Debug/net8.0/Problem1.AssemblyInfo.cs
+++ Problem1/obj/Debug/net8.0/Problem1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Problem1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0a0cb6b75e20dbbfe4aad75044c1b6428c30eae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55534b953e0ad2dac574f7e132cff3e3854c01db")]
 [assembly: System.Reflection.AssemblyProductAttribute("Problem1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Problem1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
