e89db268047ff3ca4a04fdbb3a8122e14f9d9f46
leesiyeon99 <qsqs9090@naver.com>
2025-02-27 17:00:31 +0900

    Feat : 문제14

--- Problem1/obj/Debug/net8.0/Problem1.AssemblyInfo.cs
+++ Problem1/obj/Debug/net8.0/Problem1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Problem1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00fcc6d460d072afa6cf39240a90e24fdaf14e27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0892b19ec5e96d7a1e09602f040b0964e155693")]
 [assembly: System.Reflection.AssemblyProductAttribute("Problem1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Problem1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Problem14/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Problem14
+/// <summary>
+/// 날짜: 2025.02.27
+/// 문제: 정수 n이 매개변수로 주어질 때, n의 약수를 오름차순으로 담은 배열을 return하도록 solution 함수를 완성해주세요.
+/// URL: https://school.programmers.co.kr/learn/courses/30/lessons/120897
+/// </summary>
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public class Solution
+        {
+            public int[] solution(int n)
+            {
+                List<int> answer = new List<int>();
+                int count = n / 2;
+                for (int i = 1; i <= count; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        answer.Add(i);
+                    }
+                }
+
+                answer.Add(n);
+
+                answer.Sort();
+
+                return answer.ToArray();
+            }
+        }
+    }
+}
